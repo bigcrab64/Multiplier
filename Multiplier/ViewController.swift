e50039886e0df9d9b7e62c070baa261b4726b043
Julian De La Torre <jujudlt3@gmail.com>
2022-08-26 10:56:22 -0700

    Basic multiplier

--- Multiplier/ViewController.swift
+++ Multiplier/ViewController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var firstInput: UITextField!
+    
+    @IBOutlet weak var secondInput: UITextField!
+    
+    @IBOutlet weak var Output: UILabel!
+    
+    var firstNum = ""
+    var secondNum = ""
+    
+    
+    @IBAction func multiplyButton(_ sender: UIButton) {
+        
+        firstInput.resignFirstResponder()
+        secondInput.resignFirstResponder()
+        if firstInput.text == "" || secondInput.text == ""{
+            Output.text = "Invalid Input"
+        }
+        else{
+            firstNum = firstInput.text ?? ""
+            secondNum = secondInput.text ?? ""
+                
+            let firstNumToInt = Int(firstNum) ?? 0
+            let secondNumToInt = Int(secondNum) ?? 0
+            
+            let product = firstNumToInt * secondNumToInt
+            Output.text = String(product)
+                
+            
+        }
+        
+        print(firstNum)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstInput.delegate = self
+        secondInput.delegate = self
+        
     }
 
 
